9c47f392aee7e75f5f2b63476d96462549c56fa0
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-03-01 23:07:55 +0300

    Final Tested Version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+package connectfour
+
 fun main() {
     println("Connect Four")
     println("First player's name:")
@@ -7,16 +9,16 @@ fun main() {
     println("Set the board dimensions (Rows x Columns) \nPress Enter for default (6 x 7)")
     var rows = 6
     var columns = 7
-    var failed: Boolean = true
+    var failed = true
     var input = readLine()!!
     while (input.isNotEmpty() && failed) {
         failed = false
-        if (!input.matches("[0-9]+( )*[xX]( )*[0-9]+".toRegex())){
+        if (!input.matches("\\s*[0-9]+\\s*[xX]\\s*[0-9]+\\s*".toRegex())){
             println("Invalid input")
             failed = true
         }else {
-            val rowsTest = input.split("x","X").first().replace(" ","").toInt()
-            val columnsTest = input.split("x","X").last().replace(" ","").toInt()
+            val rowsTest = input.split("x","X").first().replace(" ","").replace("\t","").toInt()
+            val columnsTest = input.split("x","X").last().replace(" ","").replace("\t","").toInt()
             if (rowsTest !in 5..9) {
                 println("Board rows should be from 5 to 9")
                 failed = true
