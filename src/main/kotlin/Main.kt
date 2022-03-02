226a6e27d9edcab2b140cdcd404d0c91bba5b63a
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-03-02 18:20:32 +0300

    added Game Logic method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
-fun printTable(gameTable: MutableList<String>): Unit {
-    val columns = gameTable[0].length/2
+fun printTable(gameTable: MutableList<String>) {
+    val columns = gameTable[0].length / 2
     val rows = gameTable.size
     for (i in 1..columns) {
         print(" $i")
     }
     print("\n")
     var under = "╚═"
-    for (i in 1 until columns){
+    for (i in 1 until columns) {
         under += "╩═"
     }
     under += "╝"
@@ -15,16 +15,28 @@ fun printTable(gameTable: MutableList<String>): Unit {
     }
     println(under)
 }
+
 fun createTable(rows: Int, columns: Int): MutableList<String> {
     var str = "║"
     val gameTable = mutableListOf<String>()
     for (i in 1..columns) {
-        str +=" ║"
+        str += " ║"
     }
     for (i in 1..rows) {
         gameTable.add(str)
     }
     return gameTable
+}
+fun addToTable(column: Int, gameBoard: MutableList<String>, toPut: Char): MutableList<String> {
+    for (i in gameBoard.lastIndex downTo 0) {
+        if (gameBoard[i][column*2 - 1] == ' ') {
+            val stringToChange = gameBoard[i].toMutableList()
+            stringToChange[column*2 - 1] = toPut
+            gameBoard[i] = stringToChange.joinToString("")
+            break
+        }
+    }
+    return gameBoard
 }
 fun main() {
     println("Connect Four")
@@ -39,12 +51,12 @@ fun main() {
     var input = readLine()!!
     while (input.isNotEmpty() && failed) {
         failed = false
-        if (!input.matches("\\s*[0-9]+\\s*[xX]\\s*[0-9]+\\s*".toRegex())){
+        if (!input.matches("\\s*[0-9]+\\s*[xX]\\s*[0-9]+\\s*".toRegex())) {
             println("Invalid input")
             failed = true
-        }else {
-            val rowsTest = input.split("x","X").first().replace(" ","").replace("\t","").toInt()
-            val columnsTest = input.split("x","X").last().replace(" ","").replace("\t","").toInt()
+        } else {
+            val rowsTest = input.split("x", "X").first().replace(" ", "").replace("\t", "").toInt()
+            val columnsTest = input.split("x", "X").last().replace(" ", "").replace("\t", "").toInt()
             if (rowsTest !in 5..9) {
                 println("Board rows should be from 5 to 9")
                 failed = true
@@ -67,4 +79,14 @@ fun main() {
     val gameBoard = createTable(rows, columns)
     printTable(gameBoard)
 
+    println("$firstName's turn:")
+    var gameInput = readLine()!!
+    var firstPlayer = true
+    while(gameInput != "end") {
+        val col = gameInput.toInt()
+
+        gameInput = readLine()!!
+        firstPlayer = !firstPlayer
+    }
+    println()
 }
