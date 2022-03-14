4ba81a3df8f8109701dad27709dcb9014cebd267
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-03-14 22:37:49 +0300

    Implemented Winning Condition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,24 +27,32 @@ fun createTable(rows: Int, columns: Int): MutableList<String> {
     }
     return gameTable
 }
+
 fun addToTable(column: Int, gameBoard: MutableList<String>, toPut: Char): MutableList<String> {
     for (i in gameBoard.lastIndex downTo 0) {
-        if (gameBoard[i][column*2 - 1] == ' ') {
+        if (gameBoard[i][column * 2 - 1] == ' ') {
+            indexs[0] = i
+            indexs[1] = column-1
             val stringToChange = gameBoard[i].toMutableList()
-            stringToChange[column*2 - 1] = toPut
+            stringToChange[column * 2 - 1] = toPut
             gameBoard[i] = stringToChange.joinToString("")
+            count++
             break
         }
     }
     return gameBoard
 }
-fun printName(firstName: String, secondName:String,isFirst: Boolean) {
+
+fun printName(firstName: String, secondName: String, isFirst: Boolean) {
     if (isFirst) {
         println("$firstName's turn:")
-    }else {
+    } else {
         println("$secondName's turn:")
     }
 }
+
+var indexs = MutableList<Int>(2) { 0 }
+var count = 0;
 fun main() {
     println("Connect Four")
     println("First player's name:")
@@ -89,34 +97,112 @@ fun main() {
     println("$firstName's turn:")
     var gameInput = readLine()!!
     var firstPlayer = true
-    while(gameInput != "end") {
+    while (gameInput != "end") {
         if (!gameInput.matches("\\d+".toRegex())) {
             println("Incorrect column number")
-            printName(firstName,secondName,firstPlayer)
+            printName(firstName, secondName, firstPlayer)
             gameInput = readLine()!!
             continue
-        }else {
+        } else {
             val column = gameInput.toInt()
             if (column !in 1..columns) {
                 println("The column number is out of range (1 - $columns)")
-                printName(firstName,secondName,firstPlayer)
+                printName(firstName, secondName, firstPlayer)
                 gameInput = readLine()!!
                 continue
-            }else {
-                if (gameBoard[0][column*2 -1] != ' ') {
+            } else {
+                if (gameBoard[0][column * 2 - 1] != ' ') {
                     println("Column $column is full")
-                    printName(firstName,secondName,firstPlayer)
+                    printName(firstName, secondName, firstPlayer)
                     gameInput = readLine()!!
                     continue
                 }
                 val toPut: Char = if (firstPlayer) 'o' else '*'
-                gameBoard = addToTable(column,gameBoard,toPut)
-                firstPlayer = !firstPlayer
+                gameBoard = addToTable(column, gameBoard, toPut)
                 printTable(gameBoard)
-                printName(firstName,secondName,firstPlayer)
+                val validate = checkGame(rows,columns,gameBoard,toPut)
+                if (validate == 1 ) {
+                    if (firstPlayer) {
+                        println("Player $firstName won")
+                    }else{
+                        println("Player $secondName won")
+                    }
+                    break
+                }
+                if (validate == -1) {
+                    break
+                }
+                firstPlayer = !firstPlayer
+                printName(firstName, secondName, firstPlayer)
                 gameInput = readLine()!!
             }
         }
     }
     println("Game over!")
+}
+
+fun converteTable(gameBoard: MutableList<String>): MutableList<String> {
+    val newTable = mutableListOf<String>()
+    for (i in gameBoard.indices) {
+        var str = ""
+        for (j in 1..gameBoard[i].lastIndex step 2) {
+            str += gameBoard[i][j]
+        }
+        newTable.add(str)
+    }
+    return newTable
+}
+
+fun checkGame(rows: Int, columns: Int, gameBoard: MutableList<String>, toPut: Char): Int {
+    if (columns * rows == count) {
+        println("It is a draw")
+        return -1
+    } else {
+        var row = indexs[0]
+        var column = indexs[1]
+        val newTable = converteTable(gameBoard)
+        // check row
+        if (toPut.toString().repeat(4) in newTable[row]) {
+            return 1
+        }
+        //check column
+        var strToCheck = ""
+        for (i in newTable) {
+            strToCheck += i[column]
+        }
+        if (toPut.toString().repeat(4) in strToCheck) {
+            return 1
+        }
+        // check diagonal
+        var row2 = row
+        var column2 = column
+        while (row2 < rows - 1 && column2 > 0) {
+            row2++
+            column2--
+        }
+        var firstDiag = newTable[row2][column2].toString()
+        while (row2 > 0 && column2 < columns - 1) {
+            row2--
+            column2++
+            firstDiag += newTable[row2][column2].toString()
+        }
+        if (toPut.toString().repeat(4) in firstDiag) {
+            return 1
+        }
+        //
+        while (row < rows - 1 && column < columns - 1) {
+            row++
+            column++
+        }
+        var secDiag = newTable[row][column].toString()
+        while (row > 0 && column > 0) {
+            row--
+            column--
+            secDiag += newTable[row][column].toString()
+        }
+        if (toPut.toString().repeat(4) in secDiag) {
+            return 1
+        }
+    }
+    return 0
 }
